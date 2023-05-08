760b168eb7eeab8c5f5029cee340357ade017808
Thura Kyaw <104015611+Thura79@users.noreply.github.com>
2023-05-08 11:59:43 +0630

    Update products.php

--- products.php
+++ products.php
@@ -11,15 +11,27 @@
         $producttext = file_get_contents("https://fakestoreapi.com/products");
 
         $products = json_decode($producttext, true);
+
+
+        function star($count) {
+    $result = "";
+    for ($i = 0; $i < $count; $i++) {
+        $result .= "* ";
+    }
+    return $result;
+}
+
     ?>
 
     <?php foreach($products as $pd): ?>
         <div class=" ">
         <img src="<?= $pd["image"] ?>" height="200" alt="">
         <p><?= $pd['title'] ?></p>
+        <p>$ <?= $pd['price'] ?></p>
+       <?= star($pd["rating"]["rate"]) ?>
         <p><?= $pd['description'] ?></p>
         </div>
-
+        <hr>
 
     <?php endforeach; ?>
 </body>
